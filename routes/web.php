78e38776c6c17b4269757a61e5db12e21b93698d
S. M Khaled Mahmud <smbdmahmud1010@gmail.com>
2024-06-06 19:37:19 +0600

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -4,20 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\FrontController;
 use App\Http\Controllers\StudentController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
-
 Route::get('/',[FrontController::class,'home'])->name('home');
-
 
 Route::get('/detail-student/{id}',[StudentController::class,'details'])->name('student-detail');
 
